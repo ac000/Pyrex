6340a20d17756437b1e0230bbf865d4bcf3671cd
Andrew Clayton <andrew@digital-domain.net>
2011-01-20 21:43:55 +0000

    Major rework
    - Refactored the code. Got all the possible rip/encode/mux commands
      together.
    
    - Added profiles which pick the relevant commands from above. There
      are initially three profiles; mpeg4, theora+ac3 (default) and
      theora+vorbis

--- pyrex.sh
+++ pyrex.sh
@@ -4,9 +4,12 @@
 function usage
 {
 	echo "Usage: pyrex.sh -t <title no> -a <audio id> -c <channels> 
-			-r <aspect ratio> -o <title name>"
+		-r <aspect ratio> [-P profile] [-T title]
+		-o <title name>"
 	echo
-	echo "Aspect ratio is in the format 16:9 or 4:3"
+	echo -e "Aspect ratio is in the format 16:9 or 4:3\n"
+	echo "Profile can be one of; mpeg4, theora+ac3 or theora+vorbis"
+	echo "It defaults to; theora+ac3"
 	
 	exit
 }
@@ -16,8 +19,7 @@ if [ "$#" -eq 0 ]; then
 	exit
 fi
 
-
-while getopts t:a:c:r:o: options; do
+while getopts "t:a:c:r:P:T:o:" options; do
 	case $options in
 		t) title=$OPTARG
 			;;
@@ -27,91 +29,98 @@ while getopts t:a:c:r:o: options; do
 			;;
 		r) aspect=$OPTARG
 			;;
+		P) profile=$OPTARG
+			;;
 		o) name=$OPTARG
 			;;
 	esac
 done
 
-
 # Validate input
-if [ ! $title ]; then
+if [[ ! $title ]]; then
 	echo "Error: Missing required option -t [dvd title number]"
 	usage
 fi
 
-if [ ! $aid ]; then
+if [[ ! $aid ]]; then
 	echo "Error: Missing required option -a [dvd audio id]"
 	usage
 fi
 
-if [ ! $channels ]; then
+if [[ ! $channels ]]; then
 	echo "Error: Missing required option -c [number of audio channels]"
 	usage
 fi
 
-if [ ! $aspect ]; then
+if [[ ! $aspect ]]; then
 	echo "Error: Missing required option -r [aspect ratio]"
 	usage
 fi
 
-if [ ! $name ]; then
+# Set the default profile
+if [[ ! $profile ]]; then
+        profile="theora+ac3"
+fi
+
+if [[ "$title2" ]]; then
+	title2="--title \"$title2\""
+fi
+
+if [[ ! $name ]]; then
         echo "Error: Missing required option -o [dvd title name]"
         usage
 fi
 
 
 #
-# MPEG4 + AC3 in Matroska
+# Commands
 #
 
 # Extract title from DVD
-#cmd1="nice mplayer dvdnav://$title -aid $aid -channels $channels -dumpstream -dumpfile $name.vob -nocache"
-#echo Executing: $cmd1
-#$cmd1
-
-# Convert AC3 into Vorbis
-##nice ffmpeg -i $name.ac3 -f s16le -acodec pcm_s16le - | oggenc -o $name.oga -q 5 -r -B 16 -C 6 -R 48000 -Q -
-
-# Encode video (MPEG4/AC3 - AVI)
-#cmd3="nice mencoder $name.vob -aid $aid -vf scale,harddup -zoom -aspect $aspect -xy 720 -o $name.avi -channels $channels -oac copy -ovc lavc -lavcopts vcodec=mpeg4"
-#echo Executing: $cmd3
-#$cmd3
-
-
-# Mux the video and audio into a Matroska container
-#cmd4="mkvmerge -o $name.mkv -A $name.avi $name.ac3"
-#cmd4="nice mkvmerge -o $name.mkv $name.avi"
-#echo Executing: $cmd4
-#$cmd4
-
-#echo "DVD rip complete."
-
-
-
-#
-# Ogg Theora + AC3 in Matroska
-#
-
-# Extract title from DVD
-cmd1="nice mplayer dvdnav://$title -aid $aid -channels $channels -dumpstream -dumpfile $name.vob -nocache"
-echo Executing: $cmd1
-$cmd1
+dvd_rip_cmd="nice mplayer dvdnav://$title -aid $aid -channels $channels -dumpstream -dumpfile $name.vob -nocache"
 
 # Extract AC3 audio
-cmd2="nice mplayer $name.vob -aid $aid -channels $channels -dumpaudio -dumpfile $name.ac3"
-echo Executing: $cmd2
-$cmd2
+extract_ac3_cmd="nice mplayer $name.vob -aid $aid -channels $channels -dumpaudio -dumpfile $name.ac3"
+
+# Convert AC3 into Vorbis
+ac3_to_vorbis_cmd="nice ffmpeg -i $name.ac3 -f s16le -acodec pcm_s16le - | oggenc -o $name.oga -q 5 -r -B 16 -C 6 -R 48000 -Q -"
+
+# Encode video (MPEG4/AC3 - AVI)
+mpeg4_enc_cmd="nice mencoder $name.vob -aid $aid -vf scale,harddup -zoom -aspect $aspect -xy 720 -o $name.avi -channels $channels -oac copy -ovc lavc -lavcopts vcodec=mpeg4"
+
+# Encode video (Theora/Vorbis - OGV)
+ogv_enc_video_cmd="nice ffmpeg2theora -o $name.ogv --no-skeleton -v 7 -a 3 $name.vob"
 
 # Encode video to Ogg Theora
-cmd3="nice ffmpeg2theora -o $name.ogv --no-skeleton -v 7 $name.vob"
-echo Executing: $cmd3
-$cmd3
+theora_enc_cmd="nice ffmpeg2theora -o $name.ogv --no-skeleton -v 7 $name.vob"
 
-# Mux the video and audio into a Matroska container
-#cmd4="mkvmerge -o $name.mkv -A $name.avi $name.ac3"
-cmd4="nice mkvmerge -o $name.mkv -A $name.ogv $name.ac3"
-echo Executing: $cmd4
-$cmd4
+# Mux the video and audio from AVI into a Matroska container
+avi_to_mkv_cmd="nice mkvmerge -o $name.mkv $name.avi"
+
+# Mux the Theora video and AC3 audio into a Matroska container
+ogv_to_mkv_cmd="nice mkvmerge -o $name.mkv -A $name.ogv $name.ac3"
+
+
+#
+# Profiles
+#
+# mpeg4 	- mpeg4 + AC3 in an AVI container
+# theora+ac3	- Theora + 5.1 AC3 in a Matroska container (default)
+# theora+vorbis	- Theora + 2.1 Vorbis in an Ogg container
+#
+
+echo "Starting DVD rip using the '$profile' profile."
+$dvd_rip_cmd
+
+if [[ $profile = mpeg4 ]]; then
+	$mpeg4_enc_cmd
+	$avi_to_mkv_cmd
+elif [[ $profile = "theora+ac3" ]]; then
+	$extract_ac3_cmd
+	$theora_enc_cmd
+	$ogv_to_mkv_cmd
+elif [[ $profile = "theora+vorbis" ]]; then
+	$ogv_enc_video_cmd
+fi
 
 echo "DVD rip complete."
-
